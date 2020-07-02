0cccba650a77a60e9483e5ac5aa14ba9f6275f51
Alexandre Pelletier <apelletier82@gmail.com>
2020-07-02 12:19:53 +0200

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
 import { ContactsRoutingModule, contactRoutes } from './contacts/contacts-routing.module';
 import { FoldersRoutingModule, folderRoutes } from './folders/folders-routing.module';
+import { homeRoutes, HomeRoutingModule } from './home/home-routing.module';
+import { HomeComponent } from './home/home/home.component';
 
-const routes: Routes = [
-]
+const emptyRoute: Route = { path: '', component: HomeComponent, pathMatch:'full' };
+const errorRoute: Route = { path:'***', component:HomeComponent, pathMatch:'full' };
+
+const routes: Routes = []
+  .concat(homeRoutes)
   .concat(contactRoutes)
-  .concat(folderRoutes);
+  .concat(folderRoutes)
+  .concat([emptyRoute, errorRoute]);
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
+    HomeRoutingModule,
     ContactsRoutingModule,
     FoldersRoutingModule
   ],
