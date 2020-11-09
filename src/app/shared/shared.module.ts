4e7a7068a97e1d1742aa780d9e2bfa38c8516ba0
Alexandre Pelletier <apelletier82@gmail.com>
2020-11-09 14:52:56 +0100

    refactoring toolbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from 'src/app/app-material.module';
 import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
-import { AppMenuCategoryItemComponent } from './components/app-menu-category-item/app-menu-category-item.component';
 import { FoldersModule } from 'src/app/folders/folders.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { NotificationComponent } from './notification/notification.component';
-import { DialogComponent } from './dialog/dialog.component';
 import { DialogModule } from './dialog/dialog.module';
 import { NotificationModule } from './notification/notification.module';
 
 
 const components = [
-  AppToolbarComponent,
-  AppMenuCategoryItemComponent
+  AppToolbarComponent
 ];
 
 const modules = [
